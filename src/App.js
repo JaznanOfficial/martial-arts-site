5bc9505bde103b2bd7e6077713a7a432b30e82ac
JaznanOfficial <jaznanofficial@gmail.com>
2021-10-04 08:05:24 +0600

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Router>
+        <Header></Header>
+        <Switch>
+        <Route exact path='/'><Home></Home></Route>
+        <Route path='/home'><Home></Home></Route>
+        <Route path='*'><NotFound></NotFound></Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import './Header.css';
 import './martial-arts-logo.png'
 
@@ -20,10 +21,10 @@ const Header = () => {
                     </Navbar.Brand>
                     <h3 className='d-inline-block align-center mt-2 text-light'>Martial Arts Academy</h3>
                     <Nav className="mx-auto custom-nav">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Services</Nav.Link>
-                        <Nav.Link href="#pricing">About Us</Nav.Link>
-                        <Nav.Link href="#pricing">Contact</Nav.Link>
+                        <Link to="/home">Home</Link>
+                        <Link to="/features">Services</Link>
+                        <Link to="/pricing">About Us</Link>
+                        <Link to="/pricing">Contact</Link>
                     </Nav>
                 </Container>
             </Navbar>
